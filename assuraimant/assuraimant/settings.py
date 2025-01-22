7ed1257fc3cf7ff40aaea7f7a71776b101b9d1c2
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-01-22 14:57:07 +0100

    clean_sheet_my_boy

--- assuraimant/assuraimant/settings.py
+++ assuraimant/assuraimant/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home',
     'assuraimant',
+    'login',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +134,6 @@ AUTH_USER_MODEL = 'assuraimant.User'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/login/'
--- assuraimant/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
-
-
-
-
-class HomeView(TemplateView):
-    template_name = 'home/home.html' #spécifie le template
