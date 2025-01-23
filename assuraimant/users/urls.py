7315d42214170e1c596596b67d936cd45865ee71
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-01-23 11:04:44 +0100

    prediction v0.1

--- assuraimant/users/urls.py
+++ assuraimant/users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
-from .views import HomeView, CreateUserViews
+from .views import HomeView, CreateUserViews, PredictionView
 from . import views
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home_view'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('signup/', CreateUserViews.as_view(), name='signup'),
-    path ("logout/", LogoutView.as_view(), name="logout")
+    path ("logout/", LogoutView.as_view(), name="logout"),
+    path ("prediction/", PredictionView.as_view(), name="prediction")
 ]
 
