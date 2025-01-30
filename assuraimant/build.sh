d38ec3a0c9b185e3b0d89ba24374612df08fcd3a
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-01-30 14:29:46 +0100

    deploiement 0.5

--- assuraimant/build.sh
+++ assuraimant/build.sh
@@ -2,10 +2,10 @@
 # Exit on error
 set -o errexit
 
-cd assuraimant  # Ajoute cette ligne si `manage.py` est dans `mysite/`
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+
+cd assuraimant  # Ajoute cette ligne si `manage.py` est dans `mysite/`
 
 # Convert static asset files
 python manage.py collectstatic --no-input
